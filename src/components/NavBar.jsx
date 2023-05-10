2e2899138ef3f8a296c3c79c96f8fb28b52e0c7d
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-09 22:58:27 -0700

    added landing page, renamed files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div className="navigation">Navigation Bar </div>
+  )
+}
+
+export default NavBar 
