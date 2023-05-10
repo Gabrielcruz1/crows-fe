b18d128c70bd712095695aeda52d97db66336fc0
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-09 23:12:06 -0700

    backgroud color text aligned

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../styles/LandingPage.css"
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="landingPage">LandingPage</div>
   )
 }
 
