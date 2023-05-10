5d8ef6e13681c52dd584b9576a9cef1c31e576ce
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-09 23:06:41 -0700

    landing page text rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import {Routes, Route} from "react-router-dom"
 import NavBar from "./components/NavBar";
+import LandingPage from "./pages/LandingPage";
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        {/* <Route path='/' element={}/>
+      <Route path='/' element={}/> */}
+      </Routes>
     </div>
   );
 }
