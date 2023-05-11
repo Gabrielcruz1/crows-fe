9977815c83e76cfb13441600742094066beaa43b
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-10 20:47:53 -0700

    navbar fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "../styles/NavBar.css"
 
 const NavBar = () => {
   return (
-    <div className="navigation">Navigation Bar </div>
+    <>
+      <div className="navigationBar">
+        <h1>LOGO</h1>
+        <Link>
+          <p>Login/Sign-up</p>
+        </Link>
+      </div>
+    </>
   )
 }
 
