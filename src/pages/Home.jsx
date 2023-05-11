fc4cafdc1379a7354a89669a8334cb6bcf18fb15
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-10 22:35:15 -0700

    content placeholders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import "../styles/Home.css"
 
 const Home = () => {
   return (
-    <div className="Home">Home</div>
+    <div className="Home">
+      <p>"Quote Brand Statement, Something Lorem Ipsum Lorem Ipsum Some random Wording"</p>
+      <p>Search Bar Here</p>
+      <h1>(Hero/ Image/ Clothing Content/)</h1>
+      <h1>(Hero/ Image/ Clothing Content/)</h1>
+    </div>
   )
 }
 
