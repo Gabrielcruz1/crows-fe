aa98f972f41b53beab118601187be9773575a337
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-24 22:55:57 -0700

    search function bug

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,16 +2,63 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 
 const SearchBar = () => {
-    return (
-        <div>SearchBar
-            <div className='searchBar'>
-                <input
-                    type="text"
-                    placeholder="Search Product Here"
-                />
-            </div>
-        </div>
+    const [searchBarInput, setSearchBarInput] = useState('');
+    const [products, setProducts] = useState([]);
+    const [filteredResults, setFilteredResults] = useState([])
 
+    const BASE_URL = "https://fakestoreapi.com/products"
+
+    const getProductData = async () => {
+        try {
+            const response = await fetch(BASE_URL);
+            console.log(response);
+            const allProducts = await response.json();
+            setProducts(allProducts);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const searchItems = (searchValue) => {
+        setSearchBarInput(searchValue);
+        console.log(searchValue);
+        if (searchBarInput !== '') {
+            const filteredData = products.filter((item) => {
+                return Object.values(item).join('').toLowerCase().includes(searchBarInput.toLowerCase())
+            });
+            console.log(filteredData)
+            setFilteredResults(filteredData)
+        } else {
+            setFilteredResults(products)
+        }
+
+    }
+
+
+    return (
+        <div className='searchBar'>
+            <input
+                type="text"
+                placeholder="Search Product Here"
+                onChange={(e) => searchItems(e.target.value)}
+            />
+            {/* {searchBarInput.length > 1 ? (
+                filteredResults.map((item) => {
+                    return (
+                        <div>
+                            <p> {item.title}</p>
+                        </div>
+                    )
+                })
+            ) : (products.map((product) => {
+                    return (
+                        <div>
+                            <p>{product.title}</p>
+                        </div>
+                    )
+                }) */}
+            </div>
     )
 }
 
