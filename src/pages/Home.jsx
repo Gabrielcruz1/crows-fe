7e565b0321f85d2fc186e4a710cf9c4649a650f0
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-24 22:13:33 -0700

    ajax fetch function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,27 @@ import { useState, useEffect } from 'react'
 const Home = () => {
   const [products, setProducts] = useState([])
 
+  const BASE_URL = "https://fakestoreapi.com/products"
+
+  const getProductData = async () =>{
+    try{  
+      const response = await fetch(BASE_URL);
+      console.log(response);
+      const allProducts = await response.json();
+      setProducts(allProducts);
+    }catch(error){
+      console.log(error)
+    }
+  } 
+
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(json => {
-        setProducts(json)
-      })
-      .catch(console.error + "error")
+    getProductData()
   }, []);
 
+  // function categorize(productName){
+  //   return productName === "men's clothing"
+  // }
 
   return (
     <div className="Home">
@@ -29,13 +40,15 @@ const Home = () => {
       {/* {products.filter(() => { })} */}
       {/* HERO IMAGE */}
       <img className='heroImage' src='https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/0f1ad52b9c71f9437f7492c8b5e1d0de.jpg' />
+      <img className='heroImage' src='https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/0f1ad52b9c71f9437f7492c8b5e1d0de.jpg' />
       <h3 className='productsHeading'>Products</h3>
       <div className='productsLayout'>
         {products.map((product, index) => {
             return (
               <div key={product.id} className="productBox">
-                <p>{product.title}</p>
-                <img className="mappedImage" src={product.image} alt={product.title} />
+                {/* {product.category === "men's clothing" ? product.title : null } */}
+                <p>{ product.title}</p>
+               <img className="mappedImage" src={product.image} alt={product.title} />
               </div>
             )
           })
