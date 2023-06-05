4467283044cca6c24b6074c43261ed0fe05c4ed7
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-06-04 21:39:34 -0700

    Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         {/* <Route path='/' element={ } />
         <Route path='/' element={ } /> */}
       </Routes>
+      <Footer />
     </div>
   );
 }
