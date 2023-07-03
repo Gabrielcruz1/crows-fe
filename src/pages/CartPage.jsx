b418f12ed8ed52bbfade2ead5a4e08d9c9028045
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-07-02 21:49:21 -0700

    Back link added to cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 const CartPage = () => {
     return (
         <div>
+            <Link to={'/home'}>Back</Link>
             <h1>CART PAGE</h1>
         </div>
     )
